aa1ecf7b6284d1fdf61153d67d607ff58be08d64
dima_dencep <dima_dencep@redlance.org>
2024-10-02 21:02:32 +0700

    Last fixes

--- emotesServer/src/main/java/io/github/kosmx/emotes/server/serializer/type/EmoteSerializerException.java
+++ emotesServer/src/main/java/io/github/kosmx/emotes/server/serializer/type/EmoteSerializerException.java
@@ -2,16 +2,16 @@ package io.github.kosmx.emotes.server.serializer.type;
 
 import java.io.PrintStream;
 
-public class EmoteSerializerException extends RuntimeException{
+public class EmoteSerializerException extends RuntimeException {
     final String type;
     final Exception internalException;
 
-    public EmoteSerializerException(String msg, String type){
+    public EmoteSerializerException(String msg, String type) {
         this(msg, type, null);
     }
 
-    public EmoteSerializerException(String msg, String type, Exception exception){
-        super(msg);
+    public EmoteSerializerException(String msg, String type, Exception exception) {
+        super(msg, exception);
         this.type = type;
         this.internalException = exception;
     }
@@ -27,9 +27,5 @@ public class EmoteSerializerException extends RuntimeException{
     @Override
     public void printStackTrace(PrintStream s) {
         super.printStackTrace(s);
-        if(internalException != null){
-            s.println("Internal error:");
-            internalException.printStackTrace(s);
-        }
     }
 }
