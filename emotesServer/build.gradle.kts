5121d5f5f9a62a2d0164393cf786e7fd0d60cdb4
BoBkiNN <bobkin.yangirov@gmail.com>
2025-01-26 01:52:35 +0500

    [server] migrate to kotlin dsl

--- emotesServer/build.gradle.kts
+++ emotesServer/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id 'java'
-    id 'java-library'
-    id 'maven-publish'
+    java
+    `java-library`
+    `maven-publish`
 }
 
 version = rootProject.mod_version
@@ -10,54 +10,38 @@ repositories {
     mavenCentral()
 }
 
-configurations {
-    dev
-}
+
+configurations.register("dev")
 
 dependencies {
-    api(project(':executor'))
+    api(project(":executor"))
 }
 
 artifacts {
-    dev(jar)
+    add("dev", tasks.jar)
 }
 
 java {
     withSourcesJar()
-    withJavadocJar()
 }
 
 
 publishing {
     publications {
-        mavenJava(MavenPublication) {
-            // add all the jars that should be included when publishing to maven
+        register<MavenPublication>("mavenJava") {
+            artifactId = "emotesServer"
 
-            artifactId = 'emotesServer'
-
-            artifact(jar)
-            artifact(sourcesJar)
-            artifact(javadocJar)
-
-            pom.withXml {
-                def depsNode = asNode().appendNode("dependencies")
-
-                def executorNode = depsNode.appendNode("dependency")
-                executorNode.appendNode("groupId", project.group)
-                executorNode.appendNode("artifactId", "emotesExecutor")
-                executorNode.appendNode("version", project.version)
-                executorNode.appendNode("scope", "compile")
-            }
+            from(components["java"])
 
             pom {
                 name = "emotesServer"
                 description = "Minecraft Emotecraft server common module"
-                url = 'https://github.com/KosmX/emotes'
+                url = "https://github.com/KosmX/emotes"
                 developers {
                     developer {
-                        id = 'kosmx'
-                        name = 'KosmX'
-                        email = 'kosmx.mc@gmail.com'
+                        id = "kosmx"
+                        name = "KosmX"
+                        email = "kosmx.mc@gmail.com"
                     }
                 }
 
@@ -69,29 +53,18 @@ publishing {
                 }
 
                 scm {
-                    connection = 'scm:git:github.com/kosmx/emotes.git'
-                    developerConnection = 'scm:git:github.com/kosmx/emotes.git'
-                    url = 'https://github.com/KosmX/emotes'
+                    connection = "scm:git:github.com/kosmx/emotes.git"
+                    developerConnection = "scm:git:github.com/kosmx/emotes.git"
+                    url = "https://github.com/KosmX/emotes"
                 }
             }
         }
     }
 
-    // select the repositories you want to publish to
     repositories {
-        // uncomment to publish to the local maven
         if (project.keysExists) {
-            repositories {
-                maven {
-                    url = 'https://maven.kosmx.dev/'
-                    credentials {
-                        username = 'kosmx'
-                        password = project.keys.kosmx_maven
-                    }
-                }
-            }
-        }
-        else {
+            kosmxRepo(project)
+        } else {
             mavenLocal()
         }
     }
